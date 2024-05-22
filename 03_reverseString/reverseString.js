5c7c63d1135900f799303e1dce532b87df4795e1
pbrotman <parkerbrotman@gmail.com>
2024-05-22 14:18:49 -0400

    Complete 03_reverseString.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(in_string) {
+    length = in_string.length;
+    out_string = "";
+    for (const idx in in_string) {
+        // Index string characters from back to front.
+        out_string += in_string[length-idx-1];
+    }
+    return out_string;
 };
 
 // Do not edit below this line
