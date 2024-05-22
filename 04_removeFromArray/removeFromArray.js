7b8197d77d800082c6c5251a04add0b4f6e84826
pbrotman <parkerbrotman@gmail.com>
2024-05-22 14:39:20 -0400

    04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inArray, ...itemsToRemove) {
+    outArray = [];
+    
+    for (const itemArray of inArray) {
+        keep = true;
+        for (const itemRemove of itemsToRemove) {
+            if (itemArray === itemRemove){
+                keep = false;
+            }
+        }
+        if(keep) {
+            outArray.push(itemArray);
+        }
+    }
+    
+    return outArray;
 };
 
 // Do not edit below this line
