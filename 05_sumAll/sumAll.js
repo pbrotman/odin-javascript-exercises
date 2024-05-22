496d31b1ce2bab40cf6ba752811990715623fe32
pbrotman <parkerbrotman@gmail.com>
2024-05-22 14:57:46 -0400

    Complete 05_sumAll.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    function isInvalid(num){
+        if ((num < 0) || !Number.isInteger(num)){
+            return true;
+        }
+    }
+    if (isInvalid(num1) || isInvalid(num2)){
+        return "ERROR"
+    }
+
+    let sum = 0;
+    let floor = Math.min(num1, num2);
+    let ceil = Math.max(num1, num2);
+
+    for (let i = floor; i < ceil+1; i++) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
