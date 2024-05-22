c729c3d9729fa1e2785e70f59d44f8261633e4f6
pbrotman <parkerbrotman@gmail.com>
2024-05-22 14:08:29 -0400

    Complete 02_repeatString.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    joined_string = "";
+    if (n<0) return "ERROR"
+    for (let i = 0; i < n; i++) {
+        joined_string += str;
+    }
+    return joined_string;
 };
 
 // Do not edit below this line
