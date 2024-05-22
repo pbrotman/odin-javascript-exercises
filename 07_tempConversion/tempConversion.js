cdfeb6c14c80b5bd4908bcb056afdef9e8466c3e
pbrotman <parkerbrotman@gmail.com>
2024-05-22 15:12:23 -0400

    Complete 07_tempConversion.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF-32) * 5/9;
+  return Number.parseFloat(tempC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = tempC * 9/5 + 32;
+  return Number.parseFloat(tempF.toFixed(1));
 };
 
 // Do not edit below this line
