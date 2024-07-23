7fb223cd4faa50a9f27014de9b2e0e10f676a483
pbrotman <parkerbrotman@gmail.com>
2024-07-23 15:12:01 -0400

    Complete 09_palindromes.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toUpperCase().replace(/[^0-9A-Z]/g,"");
+    console.log(word);
+    if(word.length <= 1){
+        return true;
+    } else {
+        if(word[0] === word[word.length-1]){
+            return palindromes(word.substr(1, word.length-2));
+        } else {
+            return false;
+        }
+    }
 };
 
 // Do not edit below this line
