fddce6c41e72745f77f3ec4d44d84e0fcf23c6ef
pbrotman <parkerbrotman@gmail.com>
2024-07-24 10:59:35 -0400

    Complete 12_findTheOldest.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestPerson = people[0];
+    people.forEach(person => {
+        if (person.yearOfDeath !== undefined){
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            person.age = (new Date()).getFullYear() - person.yearOfBirth;
+        }
+        if (person.age > oldestPerson.age) {oldestPerson = person};
+    })
+    return oldestPerson;
 };
 
 // Do not edit below this line
