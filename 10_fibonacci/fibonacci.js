8641f6c737192d85ca202e2866a065b54c22e009
pbrotman <parkerbrotman@gmail.com>
2024-07-24 10:42:46 -0400

    Complete 10_fibonacci.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if(n>2){
+        return fibonacci(n-1) + fibonacci(n-2);
+    } if(n>0){
+        return 1;
+    } if(n==0){
+        return 0;
+    } else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
